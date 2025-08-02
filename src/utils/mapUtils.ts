da16032ccdca36d05667175cc76624bb2c3f92ad
Rui Coelho <ruicoelho@ua.pt>
2025-08-02 18:46:09 +0100

    Fix lint

--- src/utils/mapUtils.ts
+++ src/utils/mapUtils.ts
@@ -3,7 +3,7 @@ import { MAP_STYLES } from '../types';
 export function getMapStylePreviewUrl(
   styleKey: keyof typeof MAP_STYLES
 ): string {
-  const previewUrls = {
+  const previewUrls: Record<keyof typeof MAP_STYLES, string> = {
     standard: 'https://a.tile.openstreetmap.org/5/16/10.png',
     dark: 'https://a.basemaps.cartocdn.com/dark_all/5/16/10.png',
     light: 'https://a.basemaps.cartocdn.com/light_all/5/16/10.png',
@@ -13,5 +13,5 @@ export function getMapStylePreviewUrl(
     vintage: 'https://a.tile.stamen.com/watercolor/5/16/10.jpg',
   };
 
-  return previewUrls[styleKey] || previewUrls.standard;
+  return previewUrls[styleKey];
 }
