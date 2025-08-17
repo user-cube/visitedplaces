734620b1c7949c9d1e8bd55a4e9a9617214268ca
Rui Coelho <ruicoelho@ua.pt>
2025-08-17 23:44:09 +0100

    fix: added trailing slash

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, // This will add trailing slashes to all pages
 };
 
 module.exports = nextConfig;
